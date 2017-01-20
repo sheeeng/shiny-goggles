5991e0f52f92734347e84c82d364941eb75abfc7
Leonard Lee <sheeeng@gmail.com>
2017-01-20 01:20:45 +0100

    Connect Internet.

--- app/src/main/java/com/example/android/movies/MainActivity.java
+++ app/src/main/java/com/example/android/movies/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.android.movies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.android.movies.utilities.NetworkUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "# MOVIES LOGS";
+    private TextView mTextViewDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextViewDisplay = (TextView) findViewById(R.id.tv_display);
+        queryMoviesDb();
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+
+    private void queryMoviesDb() {
+        if (isOnline()) {
+            Log.d(TAG, "Online!");
+            URL moviesQueryUrl = NetworkUtils.buildUrl();
+            new MoviesQueryTask().execute(moviesQueryUrl);
+        } else {
+            Log.d(TAG, "Offline!");
+        }
+    }
+
+    public class MoviesQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String moviesQueryResults = null;
+            try {
+                moviesQueryResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return moviesQueryResults;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")) {
+                mTextViewDisplay.setText(s);
+            }
+            super.onPostExecute(s);
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/example/android/movies/utilities/NetworkUtils.java
@@ -0,0 +1,70 @@
+package com.example.android.movies.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * These utilities will be used to communicate with the network.
+ */
+public class NetworkUtils {
+    static final String TAG = "# MOVIES LOGS";
+    final static String MOVIEDB_BASE_URL =
+            "http://api.themoviedb.org/3/movie/popular?";
+    final static String PARAM_API_KEY_QUERY = "api_key";
+    final static String API_KEY =
+            "";  // TODO: Replace with your own API key.
+
+    /**
+     * Builds the URL used to query MovieDb.
+     *
+     * @return The URL to use to query the weather server.
+     */
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY_QUERY, API_KEY)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+            Log.d(TAG, url.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
